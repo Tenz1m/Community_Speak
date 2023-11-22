fe526bc375af1c249c807644936c706bc2a1e97b
Tanjim islam <102029776+Tenz1m@users.noreply.github.com>
2023-11-22 20:58:06 +0600

    a

--- src/pages/Bus/location/index.tsx
+++ src/pages/Bus/location/index.tsx
@@ -1,31 +1,31 @@
-import { Box, Text ,Image} from "@chakra-ui/react";
-import { NextPage } from "next";
-import PageContentLayout from "../../../components/Layout/PageContent";
-import DigitalClock from "@/components/extra/DigitalClock";
-import MyComponent from "@/components/map/MyComponent";
-import RealtimeLocationTracker from "@/components/map/map";
+// import { Box, Text ,Image} from "@chakra-ui/react";
+// import { NextPage } from "next";
+// import PageContentLayout from "../../../components/Layout/PageContent";
+// import DigitalClock from "@/components/extra/DigitalClock";
+// import MyComponent from "@/components/map/MyComponent";
+// import RealtimeLocationTracker from "@/components/map/map";
 
-const CreateCommmunityPostPage: NextPage = () => {
+// const CreateCommmunityPostPage: NextPage = () => {
 
 
-  return (
-    <PageContentLayout maxWidth="1060px">
-     <Box color="Red" p="14px 0px" borderBottom="1px solid" borderColor="white">
-          <center>
-          <Text fontWeight={900}>Bus Live Location</Text>
-          </center>
-      {/* <MyComponent/> */}
-      <RealtimeLocationTracker/>
-     </Box>
-     <Box>
-      <DigitalClock/>
-      <br/>
-      <MyComponent/>
-      <br/>
-      <Image style={{ width: "500px", height: "300px" }} src="https://media.istockphoto.com/id/472292369/vector/school-bus.jpg?s=612x612&w=0&k=20&c=FvuZ3yPQiGFQ3DM6HXyJO8qzfA1nGkyptw3jdN6p5ds=" alt="bus" />
-      </Box>
-    </PageContentLayout>
-  );
-};
+//   return (
+//     <PageContentLayout maxWidth="1060px">
+//      <Box color="Red" p="14px 0px" borderBottom="1px solid" borderColor="white">
+//           <center>
+//           <Text fontWeight={900}>Bus Live Location</Text>
+//           </center>
+//       {/* <MyComponent/> */}
+//       <RealtimeLocationTracker/>
+//      </Box>
+//      <Box>
+//       <DigitalClock/>
+//       <br/>
+//       <MyComponent/>
+//       <br/>
+//       <Image style={{ width: "500px", height: "300px" }} src="https://media.istockphoto.com/id/472292369/vector/school-bus.jpg?s=612x612&w=0&k=20&c=FvuZ3yPQiGFQ3DM6HXyJO8qzfA1nGkyptw3jdN6p5ds=" alt="bus" />
+//       </Box>
+//     </PageContentLayout>
+//   );
+// };
 
-export default CreateCommmunityPostPage;
+// export default CreateCommmunityPostPage;
